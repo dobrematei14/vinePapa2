6e1a2d94ba141ce81a8ae103d39417ef0ac698ea
dobrematei14 <dobre.matei.ichb@gmail.com>
2019-05-18 11:21:41 +0300

    Am facut clasa biserica

---
+++ src/Biserica.java
@@ -0,0 +1,31 @@
+public class Biserica{
+    String nume;
+    Long latitudine;
+    Long longitudine;
+    String tipReligie;
+    String program;
+    int buget;
+    int rating;
+    int capacitate;
+    int oameniPrezenti;
+    public void faSlujba()
+    {
+        System.out.println("Bine ati venit la slujba de la biserica" + nume);
+    }
+    public int primesteOameni(int nrOameni)
+    {
+    if(capacitate=oameniPrezenti)
+    {
+        System.out.println("NU MAI AVEM LOCURI");
+        return 0;
+    }
+    int locuriInBiserica=capacitate - oameniPrezenti;
+    if(nrOameni>locuriInBiserica)
+    {
+        System.out.println("Din pacate au putut intra in biserica doar" + locuriInBiserica+ "oameni");
+        this.oameniPrezenti=capacitate;
+        int ramasiPeAfara=nrOameni - locuriInBiserica;
+        System.out.println("Au ramas pe afara " + ramasiPeAfara);
+    }
+    }
+}
---
+++ src/PovesteaBisericilorDinBucuresti.java
@@ -0,0 +1,8 @@
+public class PovesteaBisericilorDinBucuresti{
+    public static void main(String[] args)
+    {
+        Biserica bisericaSfIosif= new Biserica();
+        Biserica.nume= "Catedrala Romano Catolica Sf Iosfif";
+        Biserica.capacitate
+    }
+}
